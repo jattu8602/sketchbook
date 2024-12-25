c6ff440572c8ecfc96768003f5a201bec47495be
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2024-12-25 23:31:44 +0530

    hihi

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
-const drawingRoutes = require('./routes/drawing');
-const path = require('path');
 require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth')
+const drawingRoutes = require('./routes/drawing')
+const path = require('path')
 
-const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+const app = express()
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../frontend')))
 
 // Connect to MongoDB
 mongoose
@@ -18,18 +18,16 @@ mongoose
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.log(err))
 
-
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/drawing', drawingRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/drawing', drawingRoutes)
 
 // Serve frontend
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
+  res.sendFile(path.join(__dirname, '../frontend/index.html'))
+})
 
 const PORT = process.env.PORT || 5001 // Use a different port if 5000 is busy
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 )
-
